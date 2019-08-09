e1159421abc6a68c4eb8a1f9dca24b9cf8544933
yesly <34358356+yesly@users.noreply.github.com>
2019-08-08 23:47:27 -0400

    Add files via upload

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -10,10 +10,18 @@ import org.junit.Test;
 import yesly.GetGitCommits;
 
 public class test1 {
+	@Test
+	public void withGitSubModules() throws RevisionSyntaxException, NoHeadException, IOException, GitAPIException {
+		new GetGitCommits("C:/Users/yesly/Documents/git/Test/", "commits-report-with-git-sub-modules", true, false);
+	}
 
 	@Test
-	public void test() throws RevisionSyntaxException, NoHeadException, IOException, GitAPIException {
-		GetGitCommits c = new GetGitCommits("C:/Users/yesly/Documents/git/Test/");
-		c.buildReport();
+	public void notByModules() throws RevisionSyntaxException, NoHeadException, IOException, GitAPIException {
+		new GetGitCommits("C:/Users/yesly/Documents/git/Test/", "commits-report-not-by-modules", false, false);
+	}
+	
+	@Test
+	public void byModules() throws RevisionSyntaxException, NoHeadException, IOException, GitAPIException {
+		new GetGitCommits("C:/Users/yesly/Documents/git/Test/", "commits-report-by-modules", false, true);
 	}
 }
